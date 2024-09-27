702eb4c2da0c4960f0242b323ce3e416baae4d29
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-09-27 23:58:33 +0900

    [feat] 배틀신 UI 연동
    - 배틀신UI스크립트 추가

---
+++ Assets/Scripts/BattleUITextController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BattleUITextController : MonoBehaviour
+{
+    [Header("HP필드")]
+    [SerializeField] private TextMeshProUGUI playerHp;
+    [SerializeField] private TextMeshProUGUI boyHp;
+    [SerializeField] private TextMeshProUGUI enemyHp;
+
+    [Header("Attack필드")]
+    [SerializeField] private TextMeshProUGUI playerAttack;
+    [SerializeField] private TextMeshProUGUI boyAttack;
+    [SerializeField] private TextMeshProUGUI enemyAttack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHp.text = "My Hp : " + GameManager.Instance.PlayerHp;
+        boyHp.text = "boy Hp : " + GameManager.Instance.BoyHp;
+        enemyHp.text = "boy Hp : " + GameManager.Instance.EnemyHp;
+        playerAttack.text = "My Attack : " + GameManager.Instance.PlayerAttack;
+        boyAttack.text = "boy Attack : " + GameManager.Instance.BoyAttack;
+        enemyAttack.text = "boy Hp : " + GameManager.Instance.EnemyAttack;
+
+    }
+}

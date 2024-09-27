87906690899efff4721058b7e27baf152437a668
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-09-27 22:57:35 +0900

    [feat] 체력, 공격력 데이터 추가
    - 플레이어 체력 , 공격력 데이터 추가
    - 자식 체력 , 공격력 데이터 추가
    - 적 체력 , 공격력 데이터 추가

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,12 +7,59 @@ public class GameManager : MonoBehaviour
     //private static GameManager _instance;
     public static GameManager Instance {get; private set;}
     // Start is called before the first frame update
-    private int cropLevel;
-    public int CropLevel
+
+    //hp variable
+
+    private int playerHp;
+    public int PlayerHp
     {
-        get { return cropLevel; }
-        set { cropLevel = value; }
+        get { return playerHp; }
+        set { PlayerHp = value; }
     }
+
+    private int boyHp;
+    public int BoyHp
+    {
+        get { return boyHp; }
+        set { BoyHp = value; }
+    }
+
+    private int enemyHp;
+    public int EnemyHp
+    {
+        get { return EnemyHp; }
+        set { EnemyHp = value; }
+    }
+
+
+//attack variable
+    private int playerAttack;
+    public int PlayerAttack
+    {
+        get { return playerAttack; }
+        set { PlayerAttack = value; }
+    }
+
+    private int boyAttack;
+    public int BoyAttack
+    {
+        get { return boyAttack; }
+        set { BoyAttack = value; }
+    }
+
+    private int enemyAttack;
+    public int EnemyAttack
+    {
+        get { return EnemyAttack; }
+        set { EnemyAttack = value; }
+    }
+
+
+    //attack variable
+
+
+    
+    
     
     /*
     private void Awake() {

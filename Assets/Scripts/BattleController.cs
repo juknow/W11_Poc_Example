27c966785ade8e07406ac7b15f75c946a548e764
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-10-02 00:34:51 +0900

    [feat] 힐 스킬 추가
    - 힐 버튼 추가
    - 텍스트 로그 한글화
    - 턴 동안 생각 중 텍스트 추가
    - 버그 픽스 필요

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -23,6 +23,7 @@ public class BattleController : MonoBehaviour
 
     [Header("Button필드")]
     [SerializeField] private Button attackButton;
+    [SerializeField] private Button healButton;
 
     // field end
 
@@ -35,7 +36,9 @@ public class BattleController : MonoBehaviour
 
     private PlayerState playerState;
 
-    private bool isAttacking = false;
+    private bool isPlayerChecking = false;
+    private bool isboyAttacking = false;
+    private bool isboyHealing = false;
 
 
     // Start is called before the first frame update
@@ -54,13 +57,23 @@ public class BattleController : MonoBehaviour
 
         switch (playerState)
         {
+            case PlayerState.Young:
+                turnText.text = "스승 턴";
+                UpdateBattleLog($"스승 생각 중... ");
+                break;
+            case PlayerState.Middle:
+                turnText.text = "스승 턴";
+                UpdateBattleLog($"스승 생각 중... ");
+                break;
             case PlayerState.Old:
-                PlayerAttack();
+                StartCoroutine(OldStateTeacherRoutine());
                 break;
         }
     }
 
-    // Update is called once per frame
+
+
+    // Update Method
     void Update()
     {
         if (enemyHp <= 0)
@@ -69,7 +82,8 @@ public class BattleController : MonoBehaviour
             GameManager.Instance.PlayerHp -= 5;
             GameManager.Instance.PlayerAttack -= 1;
 
-            isAttacking = false;
+            isPlayerChecking = false;
+            isboyAttacking = false; isboyHealing = false;
             SceneManager.LoadScene("Home");
         }
     }
@@ -99,16 +113,23 @@ public class BattleController : MonoBehaviour
         logText.text += message + "\n";
     }
 
+
+
+    //Button Method
+
     public void AttackButton()
     {
-        if (isAttacking)
+        if (isPlayerChecking)
         {
+            isboyAttacking = true;
             attackButton.interactable = false;
+            healButton.interactable = false;
             return;
         }
 
-        isAttacking = true;
+        isPlayerChecking = true;
         attackButton.interactable = false;
+        healButton.interactable = false;
 
         switch (playerState)
         {
@@ -127,18 +148,49 @@ public class BattleController : MonoBehaviour
         }
     }
 
-    public void PlayerAttack()
+    public void HealButton()
     {
-        turnText.text = "스승 턴";
+        if (isPlayerChecking)
+        {
+            isboyHealing = true;
+            attackButton.interactable = false;
+            healButton.interactable = false;
+            return;
+        }
+
+        isPlayerChecking = true;
+        attackButton.interactable = false;
+        healButton.interactable = false;
+
+        switch (playerState)
+        {
+            case PlayerState.Young:
+                StartCoroutine(YoungHealSequence());
+                break;
+
+            case PlayerState.Middle:
+                StartCoroutine(MiddleHealSequence());
+                break;
+
+            case PlayerState.Old:
+                StartCoroutine(OldHealSequence());
+                break;
+
+        }
+    }
+
+    //Attack Method
+
+    public void TeacherAttack()
+    {
         enemyHp -= playerAttack;
-        UpdateBattleLog($"Player Attack! : {playerAttack} damage");
+        UpdateBattleLog($"스승은 공격을 선택했다! : {playerAttack} 데미지 !");
 
     }
     public void BoyAttack()
     {
-        turnText.text = "제자 턴";
         enemyHp -= boyAttack;
-        UpdateBattleLog($"Boy Attack! : {boyAttack} damage");
+        UpdateBattleLog($"제자는 공격을 선택했다! : {boyAttack} 데미지 !");
     }
     public void EnemyAttack()
     {
@@ -148,36 +200,82 @@ public class BattleController : MonoBehaviour
         if (target == 0)
         {
             playerHp -= enemyAttack;
-            UpdateBattleLog($"Enemyy Attacked Player! : {enemyAttack} damage");
+            UpdateBattleLog($"적은 스승을 공격했다! : {enemyAttack} 데미지 !");
         }
         else if (target == 1)
         {
             boyHp -= enemyAttack;
-            UpdateBattleLog($"Enemyy Attacked Boy! : {enemyAttack} damage");
+            UpdateBattleLog($"적은 제자를 공격했다! : {enemyAttack} 데미지 !");
         }
 
     }
+
+    //Heal Method
+
+    public void TeacherHeal()
+    {
+        turnText.text = "스승 턴";
+        playerHp += playerAttack;
+        UpdateBattleLog($"스승은 치료를 선택했다! : {playerAttack} 체력 회복 ! ");
+
+    }
+
+    public void BoyHeal()
+    {
+        boyHp += boyAttack;
+        UpdateBattleLog($"제자는 치료를 선택했다! : {boyAttack} 체력 회복 ! ");
+    }
+
+    // Attack Sequence Method
 
     private IEnumerator YoungAttackSequence()
     {
-        PlayerAttack();
+        TeacherAttack();
         yield return new WaitForSeconds(1f);
-        BoyAttack();
+        turnText.text = "제자 턴";
+        UpdateBattleLog($"제자 생각 중... ");
+        yield return new WaitForSeconds(1f);
+        int randomAction = Random.Range(0, 2);
+        if (randomAction == 0)
+        {
+            BoyAttack();
+        }
+        else
+        {
+            BoyHeal();
+        }
         yield return new WaitForSeconds(1f);
         if (enemyHp > 0)
         {
+            turnText.text = "적 턴";
+            UpdateBattleLog($"적 생각 중... ");
+            yield return new WaitForSeconds(1f);
             EnemyAttack();
         }
         attackButton.interactable = true;
-        isAttacking = false;
+        healButton.interactable = true;
+        isPlayerChecking = false;
+        isboyAttacking = false;
+        isboyHealing = false;
     }
     private IEnumerator MiddleAttackSequence()
     {
-        PlayerAttack();
+        TeacherAttack();
         yield return new WaitForSeconds(1f);
+        turnText.text = "제자 턴";
+        UpdateBattleLog($"제자 생각 중... ");
         attackButton.interactable = true;
-        yield return new WaitUntil(() => attackButton.interactable == false);
-        BoyAttack();
+        healButton.interactable = true;
+        yield return new WaitUntil(() => attackButton.interactable == false || healButton.interactable == false);
+        if (isboyAttacking = true)
+        {
+            BoyAttack();
+        }
+        else if (isboyHealing = true)
+        {
+            BoyHeal();
+
+        }
         yield return new WaitForSeconds(1f);
         if (enemyHp > 0)
         {
@@ -186,7 +284,9 @@ public class BattleController : MonoBehaviour
 
 
         attackButton.interactable = true;
-        isAttacking = false;
+        isPlayerChecking = false;
+        isboyAttacking = false;
+        isboyHealing = false;
     }
 
     private IEnumerator OldAttackSequence()
@@ -198,9 +298,117 @@ public class BattleController : MonoBehaviour
             EnemyAttack();
         }
         yield return new WaitForSeconds(1f);
-        PlayerAttack();
+        TeacherAttack();
         yield return new WaitForSeconds(1f);
         attackButton.interactable = true;
-        isAttacking = false;
+        healButton.interactable = true;
+        isPlayerChecking = false;
+        isboyAttacking = false;
+        isboyHealing = false;
+    }
+
+    // Heal Sequence Method
+
+    private IEnumerator YoungHealSequence()
+    {
+        TeacherHeal();
+        yield return new WaitForSeconds(1f);
+        turnText.text = "제자 턴";
+        UpdateBattleLog($"제자 생각 중... ");
+        yield return new WaitForSeconds(1f);
+        int randomAction = Random.Range(0, 2);
+        if (randomAction == 0)
+        {
+            BoyAttack();
+        }
+        else
+        {
+            BoyHeal();
+        }
+        yield return new WaitForSeconds(1f);
+        if (enemyHp > 0)
+        {
+            turnText.text = "적 턴";
+            UpdateBattleLog($"적 생각 중... ");
+            yield return new WaitForSeconds(1f);
+            EnemyAttack();
+        }
+        attackButton.interactable = true;
+        healButton.interactable = true;
+        isPlayerChecking = false;
+        isboyAttacking = false;
+        isboyHealing = false;
+    }
+
+    private IEnumerator MiddleHealSequence()
+    {
+        TeacherHeal();
+        yield return new WaitForSeconds(1f);
+        turnText.text = "제자 턴";
+        UpdateBattleLog($"제자 생각 중... ");
+        attackButton.interactable = true;
+        healButton.interactable = true;
+        yield return new WaitUntil(() => attackButton.interactable == false || healButton.interactable == false);
+        if (isboyAttacking = true)
+        {
+            BoyAttack();
+        }
+        else if (isboyHealing = true)
+        {
+            BoyHeal();
+
+        }
+        yield return new WaitForSeconds(1f);
+        if (enemyHp > 0)
+        {
+            turnText.text = "제자 턴";
+            UpdateBattleLog($"제자 생각 중... ");
+            yield return new WaitForSeconds(1f);
+            EnemyAttack();
+        }
+
+        attackButton.interactable = true;
+        healButton.interactable = true;
+        isPlayerChecking = false;
+        isboyAttacking = false;
+        isboyHealing = false;
+    }
+
+
+    // Old 상태에서의 Heal 시퀀스
+    private IEnumerator OldHealSequence()
+    {
+        BoyAttack(); // 제자가 먼저 공격
+        yield return new WaitForSeconds(1f);
+        if (enemyHp > 0)
+        {
+            EnemyAttack(); // 적이 공격
+        }
+        yield return new WaitForSeconds(1f);
+        TeacherHeal(); // 스승이 힐을 선택
+        yield return new WaitForSeconds(1f);
+        attackButton.interactable = true;
+        healButton.interactable = true;
+        isPlayerChecking = false;
+        isboyAttacking = false;
+        isboyHealing = false;
+    }
+
+
+    // Old State First Routine
+    private IEnumerator OldStateTeacherRoutine()
+    {
+        if (Random.Range(0, 2) == 0)
+        {
+            TeacherAttack();
+        }
+        else
+        {
+            TeacherHeal();
+        }
+        yield return new WaitForSeconds(1f);
+
+        turnText.text = "제자 턴";
+        UpdateBattleLog($"제자 생각 중... ");
     }
 }

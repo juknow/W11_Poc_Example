0e37a4d2c4573db10fe957c47675e75e357dfb0a
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-10-02 01:05:03 +0900

    [feat] 직업과 홈 연동

--- Assets/Scripts/UITextController.cs
+++ Assets/Scripts/UITextController.cs
@@ -14,10 +14,14 @@ public class UITextController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI playerAttack;
     [SerializeField] private TextMeshProUGUI boyAttack;
 
+    [Header("Job필드")]
+    [SerializeField] private TextMeshProUGUI playerJob;
+    [SerializeField] private TextMeshProUGUI boyJob;
+
     [Header("Bonus필드")]
     [SerializeField] private TextMeshProUGUI bonusStat;
 
-    [Header("Job필드")]
+    [Header("JobSelection필드")]
     [SerializeField] private Canvas firstTeacherJobCanvas;
     [SerializeField] private TMP_Dropdown firstTeacherJobDropDown;
     [SerializeField] private TMP_Dropdown firstBoyJobDropDown;
@@ -36,6 +40,8 @@ public class UITextController : MonoBehaviour
         boyHp.text = "boy Hp : " + GameManager.Instance.BoyHp;
         playerAttack.text = "My Attack : " + GameManager.Instance.PlayerAttack;
         boyAttack.text = "boy Attack : " + GameManager.Instance.BoyAttack;
+        playerJob.text = "스승 직업 : " + GetJobName(GameManager.Instance.TeacherJob);
+        boyJob.text = "제자 직업 : " + GetJobName(GameManager.Instance.BoyJob);
         bonusStat.text = "bonus Stats : " + GameManager.Instance.BoyBonusStat;
     }
 
@@ -44,5 +50,20 @@ public class UITextController : MonoBehaviour
         GameManager.Instance.TeacherJob = firstTeacherJobDropDown.value;
         GameManager.Instance.BoyJob = firstBoyJobDropDown.value;
         firstTeacherJobCanvas.gameObject.SetActive(false);
+    }
+
+    private string GetJobName(int jobValue)
+    {
+        switch (jobValue)
+        {
+            case 0:
+                return "전사";
+            case 1:
+                return "힐러";
+            case 2:
+                return "마법사";
+            default:
+                return "알 수 없음";
+        }
     }
 }
